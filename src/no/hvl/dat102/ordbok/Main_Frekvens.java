bcc77ce23bdaef2f4a45ce34ef28c43680d954cb
soh1961 <soh@hvl.no>
2025-03-17 14:51:45 +0100

    Fullerfører ordbok-eksemplet på forelesninga.

---
+++ src/no/hvl/dat102/ordbok/Main_Frekvens.java
@@ -0,0 +1,15 @@
+package no.hvl.dat102.ordbok;
+
+import java.util.Iterator;
+
+public class Main_Frekvens {
+	
+	public static void main(String[] args) {
+		
+		// Testdata
+		String[] ord = {"er", "det", "finn", "er", "finn", "er"};
+				
+	}
+
+}
+
